5a958c2a666b1998ef75414e5adf02e1c98d6d7d
melaniepardo <melaniepardoes@gmail.com>
2022-05-05 23:11:50 -0400

    Actualización comentarios

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,10 @@ const _ = require('lodash')
 const { v4: uuidv4 } = require('uuid')
 const getData = require('./getdata')
 
-const users = []
-const getInfo = async () => {
+const users = [] //creamos un array vacio que luego usaremos
+const getInfo = async () => { // creamos una función asincrona que retorna promesa
     const data = await getData()
-    for (const user of data) { //va a iterar la data
+    for (const user of data) { // itera la data
         const name = user.name.first //primer nombre de la persona
         const lastName = user.name.last // para el apellido
         const id = uuidv4().slice(0, 6)//va a cortar y trabajaremos en 6 caracteres // 2.  campo id único
@@ -18,17 +18,17 @@ const getInfo = async () => {
     }
     return users
 }
-http.createServer(async (req, res) => {
-    if (req.url.includes('/users')) {
+http.createServer(async (req, res) => { //se crea el servidor
+    if (req.url.includes('/users')) { //ingresamos a users
         const users = await getInfo()
-        res.write(`<html>`)
+        res.write(`<html>`) //para darle formato
         res.write(`<head><meta charSet="utf-8"/></head>`)
-        _.forEach(users, (user, i) => {//i es las veces que itera
+        _.forEach(users, (user, i) => {//i es las veces que itera con lodash (i=index)
 
             const userData = `${i+1}. Nombre: ${user.name}. Apellido: ${user.lastName}. Id: ${user.id}.Fecha: ${user.date}\n`
             res.write(`<h3>${userData}</h3>`)
-            console.log(chalk.blue.bgWhite.bold(userData))// 5. imprimir por la consola del servidor la misma lista
-// de usuarios pero con fondo blanco y color de texto azul usando el paquete Chalk.
+            console.log(chalk.blue.bgWhite.bold(userData))// 5. imprimir por la consola del servidor la lista
+// de usuarios con fondo blanco y color de texto azul usando el paquete Chalk.
         })
         res.write(`</html>`)
         res.end()
